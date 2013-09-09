7b8d11865e67027667aacb251f83d8052766e190
yenchin <yenchin@weintek.com>
2013-09-09 12:30:00 +0800

    project.clj: add dependancy

--- project.clj
+++ project.clj
@@ -1,6 +1,11 @@
 (defproject clj-netty-simple-telnet "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A simple telnet server written in Clojure with netty 3.x"
+  :url "https://github.com/coldnew/clj-netty-simple-telnet"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/tools.logging "0.2.6"]
+                 [io.netty/netty "3.6.6.Final"]]
+
+  :main clj-netty-simple-telnet.core)
