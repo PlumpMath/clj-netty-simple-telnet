50deaf0ef3a602abd708b7692b536aaac4d2b2c0
yenchin <yenchin@weintek.com>
2013-09-09 12:33:11 +0800

    core.clj: add main functions

--- src/clj_netty_simple_telnet/core.clj
+++ src/clj_netty_simple_telnet/core.clj
@@ -1,6 +1,42 @@
-(ns clj-netty-simple-telnet.core)
+(ns clj-netty-simple-telnet.core
+  (:gen-class)
+  (:import [java.net InetSocketAddress]
+           [java.util.concurrent Executors]
+           [org.jboss.netty.bootstrap ServerBootstrap]
+           [org.jboss.netty.channel Channels ChannelPipelineFactory]
+           [org.jboss.netty.channel.socket.nio NioServerSocketChannelFactory]
+           [org.jboss.netty.handler.codec.string StringEncoder StringDecoder])
+   (:use [clojure.tools.logging :as log]
+         [clj-netty-simple-telnet.handler :as handler]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn make-channel-factory
+  "Returns a Netty channel factory."
+  []
+  (NioServerSocketChannelFactory.
+   (Executors/newCachedThreadPool) (Executors/newCachedThreadPool)))
+
+(defn make-pipeline-factory
+  "Returns a Netty pipeline factory."
+  []
+  (reify ChannelPipelineFactory
+    (getPipeline [this]
+      (let [pipeline (Channels/pipeline)]
+        (.addLast pipeline "server-handler" (handler/make-handler))
+        (.addLast pipeline "string-encoder" (StringEncoder.))
+        (.addLast pipeline "string-decoder" (StringDecoder.))
+        pipeline))))
+
+(defn start-server
+  "Start a Netty server."
+  [port]
+  (let [bootstrap (ServerBootstrap. (make-channel-factory))]
+    (doto bootstrap
+      (.setPipelineFactory (make-pipeline-factory))
+      (.setOption "child.tcpNoDelay" true)
+      (.setOption "child.keepAlive" true)
+      (.bind (InetSocketAddress. port)))))
+
+;; We start server at port 5000
+;; use telnet localhost:5000 to connect this server
+(defn main []
+  (start-server 5000))
